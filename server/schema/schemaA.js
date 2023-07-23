8a9bd508613e2d31a0ce316ecbc6711a78b91afc
Jajjage <mustaphajajjage@gmail.com>
2023-07-23 16:15:33 +0100

    schema

--- server/schema/schemaA.js
+++ server/schema/schemaA.js
@@ -1,12 +1,3 @@
 const Project = require('../models/project.js');
 const Client = require('../models/client.js');
 
-const {
-    GraphQLObjectType,
-    GraphQLList,
-    GraphQLID,
-    GraphQLString,
-    GraphQLSchema,
-    GraphQLNonNull,
-    GraphQLEnumType, //this is used to make fields required
-} = require('graphql');
